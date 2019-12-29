c89a005707e6fc4ea757356f6549f9dd8d8e16ac
Stephane Bortzmeyer <stephane+framagit@bortzmeyer.org>
2019-12-29 11:16:44 +0100

    Moved to FramaGit

--- SRC/plugins/ldap/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# $Id$
-
-../../echoping -m ldap -v db.debian.org --base=dc=debian,dc=org --scope=sub
-# TODO: ldapsearch works with the bts2ldap gateway but gq and echoping fail
-#../../echoping -m ldap -v master.debian.org --port=10101 --scope=sub --base='dc=current,dc=bugs,dc=debian,dc=org' '(debbugsPackage=ldap-utils)'
